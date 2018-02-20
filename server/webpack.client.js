dd00f212de2a638ff1fd68bb85206110ae168bb0
faizanharoon1 <faizanharoon1@outlook.com>
2018-02-19 22:12:52 -0500

    Before Redux big challenges

--- server/webpack.client.js
+++ server/webpack.client.js
@@ -1,30 +1,15 @@
 const path = require("path");
+const merge = require('webpack-merge');
+const baseConfig = require('./webpack.base');
 
-module.exports = {
- 
-    // Tell webpack , the root file of our client app
-    entry: "./src/client/client.js",
-    // tell webpack where to put the output file that is generated on the browser
-    output: {
-      filename: "bundle.js",
-      path: path.resolve(__dirname, "public")
-    },
-    // Tell webpack to run babel on every file it runs through.
-    module: {
-      rules: [
-        {
-          test: /\.js?$/,
-          loader: "babel-loader",
-          exclude: /node_modules/,
-          options: {
-            presets: [
-              "react",
-              "stage-0",
-              ["env", { targets: { browsers: ["last 2 versions"] } }]
-            ]
-          }
-        }
-      ]
-    }
-  };
-  
+const config  = {
+  // Tell webpack , the root file of our client app
+  entry: "./src/client/client.js",
+  // tell webpack where to put the output file that is generated on the browser
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "public")
+  }
+};
+
+module.exports = merge(baseConfig,config);
