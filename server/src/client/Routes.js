d4a96a7550db74b0abead3fa4f0c76b3591547cb
faizanharoon1 <faizanharoon1@outlook.com>
2018-02-21 23:33:14 -0500

    User list GET using server side render and showing it on page. Impressed.

--- server/src/client/Routes.js
+++ server/src/client/Routes.js
@@ -1,12 +1,24 @@
-import React from "react";
-import { Route } from "react-router-dom";
-import Home from "./components/Home";
-
-export default () => {
-  return (
-    <div>
-      <Route exact path="/" component={Home} />
-     
-    </div>
-  );
-};
+ import React from "react";
+//  import { Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import UsersListPage,{loadData} from "./pages/UsersListPage";
+// export default () => {
+//   return (
+//     <div>
+//       <Route exact path="/" component={Home} />
+//       <Route exact path="/users" component={UsersList} />
+//     </div>
+//   );
+// };
+export default [
+  {
+    path: "/",
+    component: HomePage,
+    exact: true
+  },
+  {
+    loadData,
+    path: "/users",
+    component: UsersListPage
+  }
+];
